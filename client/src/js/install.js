a6916b5988475dbb9a922702cb9d88c695822c99
porti@Portillo17 <portillo_andres17@yahoo.com>
2024-08-31 14:56:15 -0400

    install folder

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -23,5 +23,6 @@ butInstall.addEventListener('click', async () => {
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
+    console.log('installed!');
     window.deferredPrompt = null;
 });
